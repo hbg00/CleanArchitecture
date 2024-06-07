be42b7bb0e7b356a129767905ccf0ba12de6f6a0
Uni <00hubertb00@gmail.com>
2024-06-07 20:07:52 +0200

    Fixing IdentityDbContextFactory

--- Identity/Factory/IdentityDbContextFactory.cs
+++ Identity/Factory/IdentityDbContextFactory.cs
@@ -5,29 +5,18 @@ using Identity.Data;
 
 public class IdentityDbContextFactory : IDesignTimeDbContextFactory<IdentityDbContext>
 {
-    private readonly IConfiguration _configuration;
-
-    public IdentityDbContextFactory(IConfiguration configuration)
-    {
-        _configuration = configuration;
-    }
-
     public IdentityDbContext CreateDbContext(string[] args)
     {
+        var configuration = new ConfigurationBuilder()
+              .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+              .AddJsonFile($"appsettings.Development.json", optional: true)
+              .Build();
+
         var optionsBuilder = new DbContextOptionsBuilder<IdentityDbContext>();
-        optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DbIdentity"));
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("DbIdentity"));
+
         return new IdentityDbContext(optionsBuilder.Options);
     }
 }
-/*
- *  var configuration = new ConfigurationBuilder()
-               .SetBasePath(Directory.GetCurrentDirectory())
-               .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-               .AddJsonFile($"appsettings.Development.json", optional: true)
-               .Build();
-
-            var optionsBuilder = new DbContextOptionsBuilder<RestaurantDbContext>();
-            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DbIdentity"));
-
-            return new RestaurantDbContext(optionsBuilder.Options);
- */
+  
